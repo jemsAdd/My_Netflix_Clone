638a820e56c5c7ab551123af78b01bd95b2963e4
jemsAdd <jemlinkdijitech@gmail.com>
2025-05-09 18:30:05 +0300

    footer and header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Main from './main/Main'
+
+function App() {
+  
+
+  return (
+    <>
+     <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import Home from '../pages/Home'
+import '../Index.css'
+
+
+const Main = () => {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Main
